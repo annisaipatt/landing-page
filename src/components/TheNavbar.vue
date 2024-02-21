e41666c8ae03b15b5e7ef6121a3be954b3ee3962
annisaipatt <annisaipatt@gmail.com>
2024-02-21 15:24:02 +0700

    first commit

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,3 @@
+<template>
+  <nav class="">Navbar</nav>
+</template>
---
+++ src/views/Homepage.vue
@@ -0,0 +1,9 @@
+<template>
+  <TheNavbar></TheNavbar>
+  <TheBanner></TheBanner>
+  <h1>dipa</h1>
+</template>
+<script setup>
+import TheNavbar from "@/components/TheNavbar.vue";
+import TheBanner from "../components/TheBanner.vue";
+</script>
