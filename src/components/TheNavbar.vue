ee92094e2241e5dd135fbfc60f77085cb21e5514
dipaanggra <gadaakuunn@gmail.com>
2024-02-21 16:57:26 +0700

    navbar

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,4 +1,33 @@
 <template>
-  <nav class="bg-black">Navbar</nav>
-  
+  <nav class="bg-white">
+    <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+      <div class="flex items-center justify-between h-16">
+        <div class="flex-shrink-0">
+          <!-- Logo -->
+          <div class="h-8 w-auto text-blue-500 text-2xl font-bold font-poppins">LOGO</div>
+        </div>
+        <div class="hidden md:block">
+          <div class="ml-10 flex items-baseline space-x-4">
+            <!-- Nav items -->
+            <a href="#" class="text-black-500 hover:text-blue-500 px-3 py-2 rounded-md text-sm font-medium font-poppins">Home</a>
+            <a href="#" class="text-black-500 hover:text-blue-500 px-3 py-2 rounded-md text-sm font-medium font-poppins">Services</a>
+            <a href="#" class="text-black-500 hover:text-blue-500 px-3 py-2 rounded-md text-sm font-medium font-poppins">Tools & Technology</a>
+            <a href="#" class="text-black-500 hover:text-blue-500 px-3 py-2 rounded-md text-sm font-medium font-poppins">How We Work</a>
+            <button class="px-3 py-2 bg-blue-500 text-white text-sm font-semibold rounded-md font-poppins">Get a Quote</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Mobile menu button -->
+    <div class="md:hidden">
+      <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+        <a href="#" class="text-black-500 hover:text-blue-500 block px-3 py-2 rounded-md text-base font-medium font-poppins">Home</a>
+        <a href="#" class="text-black-500 hover:text-blue-500 block px-3 py-2 rounded-md text-base font-medium font-poppins">Services</a>
+        <a href="#" class="text-black-500 hover:text-blue-500 block px-3 py-2 rounded-md text-base font-medium font-poppins">Tools & Technology</a>
+        <a href="#" class="text-black-500 hover:text-blue-500 block px-3 py-2 rounded-md text-base font-medium font-poppins">How We Work</a>
+        <button class="px-3 py-2 bg-blue-500 text-white text-sm font-semibold rounded-md font-poppins">Get a Quote</button>
+      </div>
+    </div>
+  </nav>
 </template>
