655aae31a96b9da7f28f31c5b56579aabe600d34
annisaipatt <annisaipatt@gmail.com>
2024-02-21 20:11:25 +0700

    iya

--- src/components/TheBanner.vue
+++ src/components/TheBanner.vue
@@ -1,3 +1,43 @@
 <template>
-  <h1>banner</h1>
+  <div>
+    <h1>banner</h1>
+    <div class="absolute w-[1162px] h-[598px] top-[92px] left-[164px]">
+      <div class="container mx-auto flex items-center justify-between">
+        <div class="w-1/2">
+          <div class="mb-8">
+            <h1 class="text-l mb-2 font-semibold text-[#184657]">
+              Professional Cleaning Services
+            </h1>
+            <h2 class="text-5xl mb-4 font-extrabold text-black">
+              Make Your House As Clean As Mine
+            </h2>
+            <div class="text-l mb-6 font-normal text-black">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed
+              massa ornare, pretium justo egestas, scelerisque diam. Donec quis
+              commodo ligula.
+            </div>
+            <div>
+              <button
+                class="px-3 py-2 bg-blue-500 text-white text-sm font-semibold rounded-md font-poppins"
+              >
+                Get Started Now
+              </button>
+              <button
+                class="px-3 py-2 text-black text-sm font-semibold rounded-md font-poppins"
+              >
+                Our Pricing
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="w-1/2">
+          <img
+            src="https://short.url/banner.jpeg"
+            alt=""
+            class="w-[553px] h-[598px]"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
