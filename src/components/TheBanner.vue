bf46d499251db112826a85933b20ce6bc02b96d2
annisaipatt <annisaipatt@gmail.com>
2024-02-22 08:58:10 +0700

    p

--- src/components/TheBanner.vue
+++ src/components/TheBanner.vue
@@ -7,7 +7,7 @@
             <h1 class="text-l mb-2 font-semibold text-black">
               Professional Cleaning Services
             </h1>
-            <h2 class="text-5xl mb-4 font-extrabold text-black">
+            <h2 class="text-5xl mb-4 font-bold text-black">
               Make Your House As Clean As Mine
             </h2>
             <div class="text-l mb-6 font-normal text-black">
@@ -17,12 +17,12 @@
             </div>
             <div class="flex gap-2">
               <button
-                class="px-3 py-2 bg-blue-500 text-white text-sm font-semibold rounded-md font-poppins"
+                class="px-3 py-2 bg-blue-500 text-white text-xs font-semibold rounded-md font-poppins"
               >
                 Get Started Now
               </button>
               <button
-                class="px-3 py-2 border-2 border-gray-200 text-black text-sm font-semibold rounded-md font-poppins"
+                class="px-3 py-2 border-2 border-gray-200 text-black text-xs font-semibold rounded-md font-poppins"
               >
                 Our Pricing
               </button>
